a93fba745264c8ee6c1dd45bfc3d61846d681452
MohamedTarek1993 <www.mtarek789@gmail.com>
2022-03-13 00:42:34 +0200

    12

--- js/main.js
+++ js/main.js
@@ -33,36 +33,36 @@ $(document).ready($(function () {
 }));
 // animated hamburger icon
 //    light&& dark 
-const body = document.querySelector('body');
-const toggle = document.getElementById('toggle');
-const dark = document.getElementById('dark');
-const light = document.getElementById('light');
-const logoLight=document.getElementById('logoLight');
-const logoDark=document.getElementById('logoDark');
+// const body = document.querySelector('body');
+// const toggle = document.getElementById('toggle');
+// const dark = document.getElementById('dark');
+// const light = document.getElementById('light');
+// const logoLight=document.getElementById('logoLight');
+// const logoDark=document.getElementById('logoDark');
 
-toggle.onclick = function () {
-  // toggle.classList.toggle('active-light-dark');
-  body.classList.toggle('active-body');
-  dark.classList.toggle('active-dark');
-  light.classList.toggle('active-light');
+// toggle.onclick = function () {
+//   // toggle.classList.toggle('active-light-dark');
+//   body.classList.toggle('active-body');
+//   dark.classList.toggle('active-dark');
+//   light.classList.toggle('active-light');
 
-  if(dark.classList.contains('active-dark')){
-    logoLight.style.display="none";
-    logoDark.style.display="block";
-  }
-  else{
-    logoLight.style.display="block";
-    logoDark.style.display="none";
-  }
-  dark.addEventListener( 'change', function() {
-    localStorage.setItem('dark',this.checked);
-    if(this.checked) {
-         body.classList.add('active-dark')
-    } else {
-         body.classList.remove('active-dark')     
-    }
-});
+//   if(dark.classList.contains('active-dark')){
+//     logoLight.style.display="none";
+//     logoDark.style.display="block";
+//   }
+//   else{
+//     logoLight.style.display="block";
+//     logoDark.style.display="none";
+//   }
+//   dark.addEventListener( 'change', function() {
+//     localStorage.setItem('dark',this.checked);
+//     if(this.checked) {
+//          body.classList.add('active-dark')
+//     } else {
+//          body.classList.remove('active-dark')     
+//     }
+// });
 
-}
+// }
 
 //    light&& dark
